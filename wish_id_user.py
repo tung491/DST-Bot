68b80df66e12bc3afc0c8d61b41c523029f5ed0d
tung491 <dosontung007@gmail.com>
2017-10-18 16:27:33 +0700

    update time table bot

--- wish_id_user.py
+++ wish_id_user.py
@@ -9,15 +9,16 @@ def wish_id_user():
     data = yaml.load(document)
     keys = list(data)
 
-    group1 = data[keys[0]]
-    group2 = data[keys[1]]
-    group3 = data[keys[2]]
-    group4 = data[keys[3]]
-    group5 = data[keys[4]]
+    group1 = (data[keys[0]], data[keys[1]])
+    group2 = (data[keys[2]], data[keys[3]])
+    group3 = (data[keys[4]], data[keys[5]])
+    group4 = (data[keys[6]], data[keys[7]])
+    group5 = (data[keys[8]], data[keys[9]])
+
     # .......
     # group-n = data[key[n - 1]]
 
-    return group1, group2, group3, group4, group5  # ...,group-n
+    return group1, wish_group, group2, group3, group4, group5  # ...,group-n
 
 
 def choose_wish():
@@ -25,30 +26,26 @@ def choose_wish():
 
     group1, group2, group3, group4, group5 = wish_id_user()
 
-    for id in group1.keys():
-        msg = '''Cảm ơn team Regiustea đã pha những cốc trà ngon cho mọi người
-                 Chúc ngủ ngon, {} ^^ <3
-              '''.format(group1[id])
+    for id in group1[0].keys():
+        msg = group1[1].format(group1[0][id])
 
         return_data.append((id, msg))
 
-    for id in group2.keys():
-        msg = '''Chúc mọi người trong team PYMI ngủ ngon, {}.
-                 Con bot này được viết bằng Python ^^ <3
-              '''.format(group2[id])
+    for id in group2[0].keys():
+        msg = group2[1].format(group2[0][id])
 
         return_data.append((id, msg))
 
-    for id in group3.keys():
-        msg = 'Chúc anh em trong 11A5 ngủ ngon,{} <3'.format(group3[id])
+    for id in group3[0].keys():
+        msg = group3[1].format(group3[0][id])
 
         return_data.append((id, msg))
 
-    for id in group4.keys():
-        msg = 'Chúc anh em trong team 9c ngủ ngon,{}'.format(group4[id])
+    for id in group4[0].keys():
+        msg = group4[1].format(group4[0][id])
 
         return_data.append((id, msg))
 
-    for id in group5.keys():
-        msg = 'Chúc các bro trong GHC ngủ ngon, {}'.format(group5[id])
+    for id in group5[0].keys():
+        msg = group5[1].format(group5[0][id])
         return_data.append((id, msg))
